c2f2779a79045059a3b8e400ef57345906e7463a
Eden <edench@post.bgu.ac.il>
2021-05-29 18:43:03 +0300

    Creating a new project with initial classes for part C

---
+++ src/Model/MyModel.java
@@ -0,0 +1,9 @@
+package Model;
+
+/**
+ * @author Eden_Hai
+ * @version 1.0
+ * @since 29-05-2021
+ */
+public class MyModel
+{}
---
+++ src/ViewModel/MyViewModel.java
@@ -0,0 +1,11 @@
+package ViewModel;
+
+/**
+ * @author Eden_Hai
+ * @version 1.0
+ * @since 29-05-2021
+ */
+public class MyViewModel
+{
+
+}
